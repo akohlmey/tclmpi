f45a99f7c1b7a435198abd95f4fec460ef1cea2f
Axel Kohlmeyer <akohlmey@gmail.com>
2012-05-17 17:26:24 -0400

    tests for non-blocking send/recv/wait.

--- tests/test_02.tcl
+++ tests/test_02.tcl
@@ -100,6 +100,7 @@ par_error  [list [list ::tclmpi::bcast $idata $double $master $comm] \
 set idata {0 1 3 0 1 10}
 set odata {1 -1 0 0 1 18}
 
+# logical operators
 set rdata {0 1 0 0 1 1}
 par_return [list [list ::tclmpi::allreduce $idata $int ::tclmpi::land $comm] \
                 [list ::tclmpi::allreduce $odata $int ::tclmpi::land $comm]] \
@@ -125,6 +126,7 @@ par_return [list [list ::tclmpi::allreduce $idata $int ::tclmpi::bxor $comm] \
                 [list ::tclmpi::allreduce $odata $int ::tclmpi::bxor $comm]] \
     [list $rdata $rdata]
 
+# integer
 set rdata {1 1 3 0 1 18}
 par_return [list [list ::tclmpi::allreduce $idata $int ::tclmpi::max $comm] \
                 [list ::tclmpi::allreduce $odata $int ::tclmpi::max $comm]] \
@@ -133,7 +135,6 @@ set rdata {0 -1 0 0 1 10}
 par_return [list [list ::tclmpi::allreduce $idata $int ::tclmpi::min $comm] \
                 [list ::tclmpi::allreduce $odata $int ::tclmpi::min $comm]] \
     [list $rdata $rdata]
-
 set rdata {1 0 3 0 2 28}
 par_return [list [list ::tclmpi::allreduce $idata $int ::tclmpi::sum $comm] \
                 [list ::tclmpi::allreduce $odata $int ::tclmpi::sum $comm]] \
@@ -143,7 +144,16 @@ par_return [list [list ::tclmpi::allreduce $idata $int ::tclmpi::prod $comm] \
                 [list ::tclmpi::allreduce $odata $int ::tclmpi::prod $comm]] \
     [list $rdata $rdata]
 
+# floating point
 set idata {-1e5 1.1 1.2d0 0.2e-1 0.06E+28 0x22}
+set rdata {1.0 1.1 0.0 0.02 6e+26 34.0}
+par_return [list [list ::tclmpi::allreduce $idata $double ::tclmpi::max $comm] \
+                [list ::tclmpi::allreduce $odata $double ::tclmpi::max $comm]] \
+    [list $rdata $rdata]
+set rdata {-100000.0 -1.0 0.0 0.0 1.0 18.0}
+par_return [list [list ::tclmpi::allreduce $idata $double ::tclmpi::min $comm] \
+                [list ::tclmpi::allreduce $odata $double ::tclmpi::min $comm]] \
+    [list $rdata $rdata]
 set rdata {-99999.0 0.10000000000000009 0.0 0.02 6e+26 52.0}
 par_return [list [list ::tclmpi::allreduce $idata $double ::tclmpi::sum $comm] \
                 [list ::tclmpi::allreduce $odata $double ::tclmpi::sum $comm]] \
@@ -153,10 +163,70 @@ par_return [list [list ::tclmpi::allreduce $idata $double ::tclmpi::prod $comm] 
                 [list ::tclmpi::allreduce $odata $double ::tclmpi::prod $comm]] \
     [list $rdata $rdata]
 
+# send/recv both blocking
+set idata [list 0 1 2 {3 4} 4 5 6]
+par_return [list [list ::tclmpi::send $idata $auto 1 666 $comm] \
+                [list ::tclmpi::recv $auto 0 666 $comm] ] [list {} $idata]
+set rdata [list 0 1 2 0 4 5 6]
+par_return [list [list ::tclmpi::send $idata $int 1 666 $comm] \
+                [list ::tclmpi::recv $int ::tclmpi::any_source 666 $comm] ] [list {} $rdata]
+set rdata [list 0.0 1.0 2.0 0.0 4.0 5.0 6.0]
+par_return [list [list ::tclmpi::send $idata $double 1 666 $comm] \
+                [list ::tclmpi::recv $double 0 ::tclmpi::any_tag $comm] ] [list {} $rdata]
+
+# non-blocking send / blocking recv
+set req0 ::tclmpi::req0
+set req1 ::tclmpi::req1
+set req2 ::tclmpi::req2
+set req3 ::tclmpi::req3
+set req4 ::tclmpi::req4
+set req5 ::tclmpi::req5
+set idata [list 0 1 2 {3 4} 4 5 6]
+par_return [list [list ::tclmpi::isend $idata $auto 1 666 $comm] \
+                [list ::tclmpi::recv $auto 0 666 $comm] ] [list $req0 $idata]
+set rdata [list 0 1 2 0 4 5 6]
+par_return [list [list ::tclmpi::isend $idata $int 1 666 $comm] \
+                [list ::tclmpi::recv $int ::tclmpi::any_source 666 $comm status] ] [list $req1 $rdata]
+set rdata [list 0.0 1.0 2.0 0.0 4.0 5.0 6.0]
+par_return [list [list ::tclmpi::recv $double 1 ::tclmpi::any_tag $comm status] \
+                [list ::tclmpi::isend $idata $double 0 666 $comm] ] [list $rdata $req0]
+
+# clear up all pending requests
+par_return [list [list ::tclmpi::wait $req0] \
+                [list set i 0] ] [list {} 0]
+par_return [list [list ::tclmpi::wait $req1 status] \
+                [list set i 0] ] [list {} 0]
+
+# it is no error to wait for a non-existing request
+# or twice for the same request. wait returns immediately
+par_return [list [list ::tclmpi::wait $req0] \
+                [list set i 0] ] [list {} 0]
+par_return [list [list ::tclmpi::wait $req2 status] \
+                [list set i 0] ] [list {} 0]
+
+# blocking send / non-blocking recv
+set idata [list 0 1 2 {3 4} 4 5 6]
+par_return [list [list ::tclmpi::send $idata $auto 1 666 $comm] \
+                [list ::tclmpi::irecv $auto 0 666 $comm] ] [list {} $req1]
+
+set rdata [list 0 1 2 0 4 5 6]
+par_return [list [list ::tclmpi::send $idata $int 1 666 $comm] \
+                [list ::tclmpi::irecv $int ::tclmpi::any_source 666 $comm] ] [list $rdata $req2]
+set rdata [list 0.0 1.0 2.0 0.0 4.0 5.0 6.0]
+par_return [list [list ::tclmpi::irecv $double 1 ::tclmpi::any_tag $comm] \
+                [list ::tclmpi::send $idata $double 0 666 $comm] ] [list $req2 {}]
+
 if {0} {
-# check data type conversions
-    run_return [list ::tclmpi::bcast {-1 2 +3 2.0 7 016} $int $master $comm] {-1 2 3 0 7 14}
-    run_return [list ::tclmpi::bcast  $double $master $self] {-100000.0 1.1 0.0 0.02 6e+26 34.0}
+set idata [list 0 1 2 {3 4} 4 5 6]
+par_return [list [list ::tclmpi::send $idata $auto 1 666 $comm] \
+                [list ::tclmpi::irecv $auto 0 666 $comm] ] [list $req0 $idata]
+set rdata [list 0 1 2 0 4 5 6]
+par_return [list [list ::tclmpi::isend $idata $int 1 666 $comm] \
+                [list ::tclmpi::recv $int ::tclmpi::any_source 666 $comm] ] [list $req1 $rdata]
+set rdata [list 0.0 1.0 2.0 0.0 4.0 5.0 6.0]
+par_return [list [list ::tclmpi::isend $idata $double 1 666 $comm] \
+                [list ::tclmpi::irecv $double 0 ::tclmpi::any_tag $comm] ] [list $req2 $req1]
+
 }
 
 # print results and exit
