00a61822fde26cdcc33967016cbe9c9b16296850
Axel Kohlmeyer <akohlmey@gmail.com>
2021-08-31 06:40:26 -0400

    integrated Tcl doc extraction into main CMake file

--- extract_tcl_docs.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "Extracting documentation from Tcl files")
-
-file(READ ${CMAKE_BINARY_DIR}/tclmpi.tcl TCLMPI_DOX)
-#string(REGEX MATCHALL "#X#[^\\n]*\\n" TCLMPI_DOX ${TCLMPI_TCL})
-#string(REGEX REPLACE "#X#([^\\n]*)\\n" "\\1\\n" TCLMPI_DOX ${_})
-file(WRITE ${CMAKE_BINARY_DIR}/tclmpi.dox "${TCLMPI_DOX}")
-
-file(READ ${CMAKE_BINARY_DIR}/harness.tcl HARNESS_DOX)
-file(WRITE ${CMAKE_BINARY_DIR}/harness.dox "${HARNESS_DOX}")
